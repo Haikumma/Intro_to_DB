1d7f270f0cc37a9cb34082bdbdec37a14b402e90
haikuu <156611902+Haikumma@users.noreply.github.com>
2024-10-21 16:54:33 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,6 +1,4 @@
 DROP SCHEMA IF EXISTS alx_book_store ;
-CREATE SCHEMA alx_book_store ;
-USE alx_book_store ;
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
